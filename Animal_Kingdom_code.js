ba69fc9f2a6ff7fc1358edc2adb3efb731ab177c
Couragenwanduka <156565155+Couragenwanduka@users.noreply.github.com>
2024-02-04 15:08:50 +0100

    Update Animal_Kingdom_code.js

--- Animal_Kingdom_code.js
+++ Animal_Kingdom_code.js
@@ -19,8 +19,9 @@ class Animal{
 }
 
 class Arthropod extends Animal{
-  constructor(name,backbone,habitant, species,characteristics){
+  constructor(name,backbone,habitant, species,characteristics,numberOfLeg){
     super(name,backbone,habitant, species,characteristics)
+    this.numberOfLeg=numberOfLeg;
     this.arthropodArray=[];
   }
    move(animal){
@@ -29,12 +30,16 @@ class Arthropod extends Animal{
 reproduce(animal){
   animal.arthropodArray.push(`Reproduction: They mate, joining the tips of their abdomens, and the male passes sperm to the female in order to fertilize her eggs. The female then lays her eggs on plants or on the ground`);
 };
+fly(animal){
+  animal.arthropodArray.push(`${this.name} has beutiful wings for flying`)
+}
 }
 
 class Fish extends Animal{
-  constructor(name,backbone,habitant, species,characteristics){
+  constructor(name,backbone,habitant, species,characteristics,waterType){
     super(name,backbone,habitant, species,characteristics)
-    this.fishArray=[]
+    this.waterType=waterType;
+    this.fishArray=[];
   }
   move(fish){
  fish.fishArray.push(" Movement:Fish also have strong muscles which make the front part of the body curve to one side and the tail fins part swing on the opposite side in the water")
@@ -43,22 +48,31 @@ class Fish extends Animal{
    fish.fishArray.push("Reproduction: by bearing live young or by laying eggs")
   }
 
+  oxygen(fish){
+    fish.fishArray.push(`${this.name} never stops swimming less they die`)
+  }
+
 }
 class Amphibia extends Animal{
-  constructor(name,backbone,habitant, species,characteristics){
+  constructor(name,backbone,habitant, species,characteristics,coldBlooded){
     super(name,backbone,habitant, species,characteristics);
-    this.amphibiaArray=[]
+    this.coldBlooded=coldBlooded;
+    this.amphibiaArray=[];
   }
   move(frog){
    frog.amphibiaArray.push(" Movement: Different frog species use several methods of moving around, including jumping, walking, running, climbing, swimming, burrowing, and gliding")
-  }
+  };
   reproduce(frog){
    frog.amphibiaArray.push("Reproduction: All frogs reproduce sexually, and all hatch from eggs")
+  };
+  herinate(frog){
+    frog.amphibiaArray.push(`${this.name} herinates during winter`)
   }
 }
 class Reptiles extends Animal{
-  constructor(name,backbone,habitant, species,characteristics){
+  constructor(name,backbone,habitant, species,characteristics, hasScales){
     super(name,backbone,habitant, species,characteristics);
+    this.hasScales=hasScales;
     this.reptilesarray=[];
   }
   move(tortoies){
@@ -67,22 +81,30 @@ class Reptiles extends Animal{
   reproduce(tortoies){
     tortoies.reptilesarray.push(" Reproduction: They lay egg");
   }
+  lifeSpan(tortoies){
+    tortoies.reptilesarray.push(`${this.name} has a live expectancy of more than 200years`)
+  }
 }
 class AVES extends Animal{
-  constructor(name,backbone,habitant, species,characteristics){
+  constructor(name,backbone,habitant, species,characteristics,wings){
     super(name,backbone,habitant, species,characteristics);
-    this.avesArray=[]
+    this.wings=wings;
+    this.avesArray=[];
   }
   move(aves){
     aves.avesArray.push(" Movement:These large birds move through the landscape primarily by soaring—a style of flying where they hold their wings outward and rarely flap, saving them considerable energy")
-  }
+  };
   reproduce(aves){
     aves.avesArray.push(" reprodection: They lay and hatch their eggs")
+  };
+  hunters(aves){
+    aves.avesArray.push(`${this.name} has a perfect eyesight and never misses to catch a prey  `)
   }
 }
 class Mammal extends Animal{
-  constructor(name,backbone,habitant, species,characteristics){
+  constructor(name,backbone,habitant, species,characteristics,furColor){
     super(name,backbone,habitant, species,characteristics)
+    this.furColor=furColor;
     this.mammalArray=[];
   }
   move(cat){
@@ -93,31 +115,37 @@ class Mammal extends Animal{
   }
   accessReproduction(cat){
     this.#reproduce(cat)
-  }
+  };
+
 }
 
 const butterfly= new Arthropod();
-const butterfly1= new Arthropod("Butterfly","No backbone","Forests","Painted Lady","They have two pairs of membranous (transparent) wings that are covered with tiny scales which give colour, rigidity, and strengthIt consists of large compound eyes having many light-sensitive lenses, both the eyes have their own refractive system and each form its own portion of the imageIt generally has a clavated antenna and sometimes it can even have a clubbed antenna");
+const butterfly1= new Arthropod("Butterfly","No backbone","Forests","Painted Lady","They have two pairs of membranous (transparent) wings that are covered with tiny scales which give colour, rigidity, and strengthIt consists of large compound eyes having many light-sensitive lenses, both the eyes have their own refractive system and each form its own portion of the imageIt generally has a clavated antenna and sometimes it can even have a clubbed antenna",6);
 const fish= new Fish();
-const fish1= new Fish("Fish","Backbones are Present","water","tilapia","Fish have gills that allow them to absorb oxygen from water and release carbon dioxide. Some fish in severely oxygen-depleted environments have developed lungs too. All fish also have a swim bladder - this is an organ filled with air that helps them maintain a stable buoyancy in the water (which means they do not sink!), it also allows them to sleep. Fins provide motion and manoeuvrability to fish, and the most common types are the tail fin, side fins, dorsal fins and anal fins.");
+const fish1= new Fish("Fish","Backbones are Present","water","tilapia","Fish have gills that allow them to absorb oxygen from water and release carbon dioxide. Some fish in severely oxygen-depleted environments have developed lungs too. All fish also have a swim bladder - this is an organ filled with air that helps them maintain a stable buoyancy in the water (which means they do not sink!), it also allows them to sleep. Fins provide motion and manoeuvrability to fish, and the most common types are the tail fin, side fins, dorsal fins and anal fins.","Fresh Water");
 const frog= new Amphibia();
-const frog1= new Amphibia("Frog","Backbone present","Water and Land","Rain Frog","frogs have protruding eyes, no tail, and strong, webbed hind feet that are adapted for leaping and swimming. They also possess smooth, moist skins. Many are predominantly aquatic, but some live on land, in burrows, or in trees");
+const frog1= new Amphibia("Frog","Backbone present","Water and Land","Rain Frog","frogs have protruding eyes, no tail, and strong, webbed hind feet that are adapted for leaping and swimming. They also possess smooth, moist skins. Many are predominantly aquatic, but some live on land, in burrows, or in trees","true");
 const tortoies= new Reptiles();
-const tortoies1= new Reptiles("Tortoies","Backbone present","Land","Tent Tortoise","A few other types of tortoise species can have nearly spherical shells with a flat base. The tortoise can be easily recognised by their unique hind-limb anatomy. Their hind-limbs are elephentine or cylindrical. Each digit in their forefeet and their hind feet contains two or fewer phalanges.");
+const tortoies1= new Reptiles("Tortoies","Backbone present","Land","Tent Tortoise","A few other types of tortoise species can have nearly spherical shells with a flat base. The tortoise can be easily recognised by their unique hind-limb anatomy. Their hind-limbs are elephentine or cylindrical. Each digit in their forefeet and their hind feet contains two or fewer phalanges.","true");
 const eagle= new AVES()
-const eagle1= new AVES("Eagle","Backbone Present", "Mountains","Bald Head eagle","Eagles have powerful vision.Eagles are fearless.Eagles are tenacious.Eagles are high flyers.Eagles nurture their young.Eagles Nurture their younger one.Eagles posses Vitality.Eagles Never Eat Dead Meat.")
+const eagle1= new AVES("Eagle","Backbone Present", "Mountains","Bald Head eagle","Eagles have powerful vision.Eagles are fearless.Eagles are tenacious.Eagles are high flyers.Eagles nurture their young.Eagles Nurture their younger one.Eagles posses Vitality.Eagles Never Eat Dead Meat.","2 powerful wings")
 const cat= new Mammal();
-const cat1= new Mammal("Cat","Backbone Present","Domastic","Orange cat","small size, social nature, obvious body language, love of play, and high intelligence. Captive Leopardus cats may also display affectionate behavior toward humans but were not domesticated")
+const cat1= new Mammal("Cat","Backbone Present","Domastic","Orange cat","small size, social nature, obvious body language, love of play, and high intelligence. Captive Leopardus cats may also display affectionate behavior toward humans but were not domesticated","orange fur")
 butterfly.move(butterfly1);
 butterfly.reproduce(butterfly1);
+butterfly.fly(butterfly1);
 fish.move(fish1);
 fish.reproduce(fish1);
+fish.oxygen(fish1);
 frog.move(frog1);
 frog.reproduce(frog1);
+frog.herinate(frog1)
 tortoies.move(tortoies1);
 tortoies.reproduce(tortoies1);
+tortoies.lifeSpan(tortoies1)
 eagle.move(eagle1);
 eagle.reproduce(eagle1);
+eagle.hunters(eagle1)
 cat.move(cat1);
 cat.accessReproduction(cat1);
 console.log(butterfly1);
